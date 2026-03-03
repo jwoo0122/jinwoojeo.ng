ca964546507e0eabbf57dba8e4687d65acd7d3b6
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2026-03-04 04:19:51 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,28 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jinwoojeo.ng",
+
   integrations: [
     mdx(),
     sitemap(),
     icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
   ],
+
   markdown: {
     shikiConfig: {
       theme: "aurora-x",
       wrap: true,
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss({
+    })],
   },
 });
