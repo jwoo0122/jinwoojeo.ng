fcbd30c5895f6057f1e8f4ca1433bd8cab9a3769
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2026-04-05 10:28:50 +0900

    redesign

--- js/components.js
+++ js/components.js
@@ -1,2 +1,3 @@
+import './components/site-layout.js';
 import './components/quote-block.js';
 import './components/blog-image.js';
---
+++ js/components/site-layout.js
@@ -0,0 +1,57 @@
+export class SiteLayout extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          width: 100%;
+          max-width: 40rem;
+          padding: 1rem;
+        }
+        .header {
+          margin-bottom: 2rem;
+        }
+        .site-title {
+          font-weight: 700;
+        }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+        .nav-link {
+          display: block;
+          margin: 0.75rem 0;
+          color: var(--menu-color);
+        }
+        .footer {
+          margin-top: 4rem;
+          font-size: 0.875rem;
+          color: var(--footer-color);
+        }
+        .divider {
+          width: 100%;
+          margin: auto;
+          margin-bottom: 48px;
+          height: 1px;
+          background-color: rgba(0, 0, 0, 0.1);
+        }
+      </style>
+      <div class="header">
+        <span class="site-title">Jinwoo Jeong</span>
+        <a class="nav-link" href="/">About</a>
+        <a class="nav-link" href="/posts">Posts</a>
+        <a class="nav-link" href="https://github.com/jwoo0122" target="_blank">GitHub</a>
+      </div>
+      <div class="divider">
+      </div>
+      <slot></slot>
+      <footer class="footer">
+        &copy; <span id="year"></span> Jinwoo Jeong. All rights reserved.
+      </footer>
+    `;
+    shadow.getElementById('year').textContent = new Date().getFullYear();
+  }
+}
+customElements.define('site-layout', SiteLayout);
